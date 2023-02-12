c224b9a082930c49f335ba6fc50d7cc7ef15ffb8
kmarek973@gmail.com <kmarek973@gmail.com>
2023-02-12 23:05:48 +0100

    added meta tag

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,7 @@ export default function App({ Component, pageProps }) {
     <Layout>
       <Head>
         <title>mmoportal</title>
+        <meta name="viewport" content="width=device-width initial-scale=1" />
       </Head>
       <Component {...pageProps} />
     </Layout>

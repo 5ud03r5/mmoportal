9c45fc978732ab4e8bf78ab335134730e7d7dc81
kmarek973@gmail.com <kmarek973@gmail.com>
2023-02-12 22:55:47 +0100

    changed favicon and title

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,12 @@
 import "@/styles/globals.css";
 import Layout from "@/components/Layout";
+import Head from "next/head";
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <title>mmoportal</title>
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );

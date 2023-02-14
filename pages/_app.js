2ef898b3c06bda73a3062bff27c3174eeddfe8a1
kmarek973@gmail.com <kmarek973@gmail.com>
2023-02-14 21:50:57 +0100

    added nprogress

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
 import "@/styles/globals.css";
+import "@/styles/nprogress.css";
 import Layout from "@/components/Layout";
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+import NProgress from "nprogress";
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.events.on("routeChangeStart", () => NProgress.start());
+    router.events.on("routeChangeComplete", () => NProgress.done());
+    router.events.on("routeChangeError", () => NProgress.done());
+  }, []);
   return (
     <Layout>
       <Head>
